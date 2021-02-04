be59e81890cb4189cff51827ff82b7a02dbfeaaa
Adithya <53051177+Pro-Grammar05@users.noreply.github.com>
2021-02-04 08:21:24 +0530

    Update covid.py

--- covid.py
+++ covid.py
@@ -5,6 +5,7 @@ r = requests.get('https://www.worldometers.info/coronavirus/countries-where-coro
 soup = bs(r.content, features = 'html.parser')
 
 country = input("\nEnter a country: ")
+country = country.capitalize()
 
 head = soup.find("tr")
 titles = head.find_all("th")
@@ -17,11 +18,11 @@ table = soup.find("table")
 rows = table.find_all("tr")
 
 for row in rows:
-    names = row.find_all(attrs={'style': 'font-weight: bold; font-size:16px; text-align:left; padding-left:5px; padding-top:10px; padding-bottom:10px'})
+    names = row.find_all(attrs = {'style': 'font-weight: bold; font-size:16px; text-align:left; padding-left:5px; padding-top:10px; padding-bottom:10px'})
     
     for name in names:
         if country in name.text:
             print(f'\n{menu[0]}: {name.text}')
-            print(f'{menu[1]}: {name.find_next_sibling("td").text}')
-            print(f'{menu[2]}: {name.find_next_sibling("td").find_next_sibling("td").text}')
-            print(f'{menu[3]}: {name.find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text}')
+            print(f'Active Covid {menu[1]}: {name.find_next_sibling("td").text}')
+            print(f'{menu[2]} due to Covid: {name.find_next_sibling("td").find_next_sibling("td").text}')
+            print(f'{menu[3]} of the world: {name.find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text}')
